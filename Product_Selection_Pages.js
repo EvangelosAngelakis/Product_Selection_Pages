b813f9ec7174fe7e22db04cad3aa120e18ca961c
Evangelos Angelakis <Evangelosangel@gmail.com>
2024-11-26 15:01:47 +0000

    Added Buttons & started building JS

--- Product_Selection_Pages.js
+++ Product_Selection_Pages.js
@@ -1,3 +1,19 @@
+const links = [
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+    {text:'', href:'', src:''},
+];
+
+let linkIndex = 0;
+
+document.getElementById('dButton').addEventListener('click', function(){
+linkIndex = (linkIndex + 1) % links.length;
+});
 
 function resetPage(){
     location.reload();
